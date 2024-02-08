ee4e9cc127c27342e98f04bb10dd8ad6f7dccb24
Brian <brian@quantrocket.com>
2024-02-07 16:08:17 -0800

    use initial_universe

--- fundamental_factors/universe.py
+++ fundamental_factors/universe.py
@@ -1,4 +1,4 @@
-# Copyright 2023 QuantRocket LLC - All Rights Reserved
+# Copyright 2024 QuantRocket LLC - All Rights Reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,19 +14,28 @@
 
 from zipline.pipeline import master, EquityPricing
 
-def BaseUniverse():
+def CommonStocks():
     """
     Return a Pipeline filter that is limited to domestic common stocks, excluding
-    secondary shares.
+    secondary shares. This filter is intended to be used as the initial_universe
+    of the Pipeline.
     """
     category = master.SecuritiesMaster.sharadar_Category.latest
-    base_universe = (
+    common_stocks = (
         # domestic common stocks
         category.has_substring("Domestic Common")
         # no secondary shares
         & ~category.has_substring("Secondary")
     )
-    base_universe = (EquityPricing.volume.latest > 0).all(21, mask=base_universe)
+    return common_stocks
+
+def BaseUniverse():
+    """
+    Return a Pipeline filter that excludes illiquid stocks and penny stocks.
+    This filter is intended to be used as the screen of the Pipeline or the
+    mask of factors in the Pipeline.
+    """
+    base_universe = (EquityPricing.volume.latest > 0).all(21)
     base_universe = (EquityPricing.close.latest > 1.00).all(21, mask=base_universe)
-    
+
     return base_universe
